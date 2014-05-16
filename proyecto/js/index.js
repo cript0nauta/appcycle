927d289e82cb901e8ec731b9a11e80d4be54a900
Matías Lang <shareman1204@gmail.com>
2014-05-16 18:30:00 -0300

    js para el index

---
+++ proyecto/js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $.getJSON('get_categorias', 
+        function(categorias){
+            for(i = 0; i < categorias.length; i++){
+                json_categoria = categorias[i];
+                categoria = $('<div>').addClass('categoria');
+                categoria.append($('<h3>').text(json_categoria.nombre));
+                categoria.append($('<img>').attr('src', json_categoria.imagen));
+                categoria.appendTo($('#categorias'));
+            }
+        });       
+});
+
