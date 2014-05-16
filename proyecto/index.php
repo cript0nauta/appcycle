e5a32123207587f8481d3907859ab93512413be9
antoniomateo54 <amateo@mae.com.ar>
2014-05-16 17:02:22 -0300

    cambio en index

--- proyecto/index.php
+++ proyecto/index.php
@@ -1,20 +1,16 @@
 <?php
 	session_start();
-	if(!isset($_SESSION['admin']) || !$_SESSION['admin'][0]){
+
 	require_once('php/forms.php');
 	require_once('php/functions.php');
-	$forms = new forms();
-	$form ="<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
+	$form = new forms();
+	$content ="<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
 	<html xmlns='http://www.w3.org/1999/xhtml'>
-	<head>".$forms -> contenido_head_index()."</head>
-		<body class='color_central'>
-			<div id='logocentral' align='center'></div>
-			".$forms -> form_login().$forms -> form_div_msg()."
-		</body>
+	<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+	<meta http-equiv='pragma' content='no-cache' />
+	<head>".$form -> contenido_head()."</head>
+	<body>".$form->content_cabecera()."</body>
 	</html>";
 	}
-	else{
-	$form ="<script>self.location.href='principal/';</script>";
-	}
-	echo $form;
+	echo $content;
 	?>
