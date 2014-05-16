6d54df8ee6658109a0cda35620831fc1791004f7
Matías Lang <shareman1204@gmail.com>
2014-05-16 18:52:45 -0300

    Imagen circular que se carga vía JSON para cada categoría

--- proyecto/js/index.js
+++ proyecto/js/index.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
                 json_categoria = categorias[i];
                 categoria = $('<div>').addClass('categoria');
                 categoria.append($('<h3>').text(json_categoria.nombre));
-                categoria.append($('<img>').attr('src', json_categoria.imagen));
+                categoria.append($('<img>').addClass('img-circle').attr('src', json_categoria.imagen));
                 categoria.appendTo($('#categorias'));
             }
         });       
