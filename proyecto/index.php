20237fbabdc892b244ed87cfd811323abe3cb439
Matías Lang <shareman1204@gmail.com>
2014-05-16 18:26:43 -0300

    cambios al index

--- proyecto/index.php
+++ proyecto/index.php
@@ -8,8 +8,23 @@
 	<html xmlns='http://www.w3.org/1999/xhtml'>
 	<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
 	<meta http-equiv='pragma' content='no-cache' />
+    <link rel='stylesheet' href='/css/index.css' />
 	<head>".$form -> contenido_head2()."</head>
-	<body>".$form->content_cabecera()."</body>
-	</html>";
+    <body>".$form->content_cabecera().'</body>
+
+    <div class="jumbotron">
+        <div class="container">
+            <h1>AppCycle</h1>
+            <p>Texto de prueba</p>
+        </div>
+    </div> 
+
+    <div class="categoria"><img src="http://www.antoniomateo.com.ar/proyectos/appcycle/graficos/LogoSecundario.png" alt="" class="categoria" /></div>
+    <div class="categoria"><img src="http://www.antoniomateo.com.ar/proyectos/appcycle/graficos/LogoSecundario.png" alt="" class="categoria" /></div>
+    <div class="categoria"><img src="http://www.antoniomateo.com.ar/proyectos/appcycle/graficos/LogoSecundario.png" alt="" class="categoria" /></div>
+    <div class="categoria"><img src="http://www.antoniomateo.com.ar/proyectos/appcycle/graficos/LogoSecundario.png" alt="" class="categoria" /></div>
+    <div class="categoria"><img src="http://www.antoniomateo.com.ar/proyectos/appcycle/graficos/LogoSecundario.png" alt="" class="categoria" /></div>
+
+	</html>';
 	echo $content;
 	?>
